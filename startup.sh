3d647e01a9751d646b693c8e7c6acc99ada8117a
dabo02 <dabo_02@live.com>
2017-05-27 01:05:28 -0400

    Completed documentation worked on startup script

--- startup.sh
+++ startup.sh
@@ -1,3 +1,16 @@
+#!/bin/bash
+# /etc/init.d/startup
+
+### BEGIN INIT INFO
+# Provides:          startup
+# Required-Start:    $ALL
+# Required-Stop:
+# Default-Start:     2 3 5
+# Default-Stop:      0 1 6
+# Short-Description: Run Video Player as Daemon
+# Description:       This script starts the server and worker for the Video Player App
+### END INIT INFO
+
 python3 /home/pi/Desktop/Upwork_Tom_VideoShowroom/Back-End.py &
 
 sleep 3
